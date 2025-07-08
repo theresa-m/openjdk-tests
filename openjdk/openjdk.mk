47827202463d12f172025f883523623343ec6a5f
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-07-08 13:43:11 -0400

    increase timeout 10x

--- openjdk/openjdk.mk
+++ openjdk/openjdk.mk
@@ -108,15 +108,15 @@ JTREG_IGNORE_OPTION = -ignore:quiet
 JTREG_BASIC_OPTIONS += $(JTREG_IGNORE_OPTION)
 # riscv64 machines aren't very fast (yet!!)
 ifeq ($(ARCH), riscv64)
-	JTREG_TIMEOUT_OPTION = -timeoutFactor:30
+	JTREG_TIMEOUT_OPTION = -timeoutFactor:300
 # aarch64 linux requires extra time, set timeoutFactor to 12
 else ifneq ($(filter linux_aarch64, $(SPEC)),)
-	JTREG_TIMEOUT_OPTION =  -timeoutFactor:30
+	JTREG_TIMEOUT_OPTION =  -timeoutFactor:300
 # Multiple by 8 the timeout numbers, except on zOS use 2
 else ifeq ($(OS),OS/390)
-	JTREG_TIMEOUT_OPTION =  -timeoutFactor:30
+	JTREG_TIMEOUT_OPTION =  -timeoutFactor:300
 else
-	JTREG_TIMEOUT_OPTION =  -timeoutFactor:30
+	JTREG_TIMEOUT_OPTION =  -timeoutFactor:300
 endif
 
 JTREG_BASIC_OPTIONS += $(JTREG_TIMEOUT_OPTION)
